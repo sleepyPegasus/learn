98c275ff76e831436fa30246bef8602310b43fc5
gokyle <710873533@qq.com>
2018-03-19 19:29:56 +0800

    first commit

--- app/src/main/java/com/learn/chengzhenxing/reminder/ReminderActivity.java
+++ app/src/main/java/com/learn/chengzhenxing/reminder/ReminderActivity.java
@@ -2,12 +2,30 @@ package com.learn.chengzhenxing.reminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ReminderActivity extends AppCompatActivity {
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+
+        mListView = (ListView)findViewById(R.id.reminders_list_view);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                //context
+                this,
+                //layout
+                R.layout.reminders_row,
+                //row
+                R.id.row_text,
+                //data with bogus data to test our listview
+                new String[]{
+                        "first record","second record","third record","fourth record","fifth record"}
+        );
+        mListView.setAdapter(arrayAdapter);
     }
 }
